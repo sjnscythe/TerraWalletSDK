0432c1bc94f21d260e1e50622231123edc7dfe03
scythe <scytheabhi97@gmail.com>
2024-02-16 11:36:50 +0530

    first commit

---
+++ Example/Pods/Target Support Files/Pods-TerraWalletSDK_Tests/Pods-TerraWalletSDK_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TerraWalletSDK_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TerraWalletSDK_Tests
+@end
---
+++ Example/Pods/Target Support Files/TerraWalletSDK/TerraWalletSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TerraWalletSDK : NSObject
+@end
+@implementation PodsDummy_TerraWalletSDK
+@end
